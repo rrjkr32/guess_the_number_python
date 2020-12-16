ccd08441b351d6f5b05e34b6081b0e0c57c15b61
rrjkr32 <rrjkr32@gmail.com>
2020-12-16 13:27:49 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,25 @@
+      from random import *
+            def valip(ip):
+                while   not ip.isdigit() or not (1<=int(ip)<=100):
+                         ip=input("That is not Valid,Guess a number between 1 and 100:\n")
+                return ip
+            def main():
+                    ip=input("Guess a number between 1 and 100:\n")
+
+                    op=randint(1,100)
+                    cnt=1
+
+                    ip=valip(ip)
+
+                    while int(ip)!=op:
+                        ip=valip(ip)
+                        if  int(ip)==op:
+                            break
+                        if int(ip)>op:
+                            ip=input("Too high ,Guess again:\n")
+                        else:
+                            ip=input("Too low ,Guess again:\n")
+                        cnt+=1
+                    print("You won in {} times\nThe number was {}\nThanks for playing!".format(cnt,op))
+
+            main()
